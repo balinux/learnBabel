e592e220c4255d5b3cbfb373b568a83a2d46cf16
balinux <ryojuniyantara@gmail.com>
2018-11-01 14:28:01 +0800

    create person class

---
+++ src/person.js
@@ -0,0 +1,14 @@
+class Person{
+    constructor(name) {
+        this.name = name
+    }
+
+    getName(){
+        return this.name + ' mengatakan hallo kepada anda semua';
+    }
+}
+
+// export default Person;
+
+// console.log(new Person('balinux').getName());
+
